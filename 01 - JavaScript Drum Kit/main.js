810f82ec5624dff8f5c4b78cff87d12c479a6b1e
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-03-05 23:02:15 -0700

    finished product that links the html file to js :ws

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown', function(e) {
-    let audio = document.querySelector(`audio[data.key='${e.keyCode}']`);
-    let key
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
+
+    key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
+}
 
-})
+let keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
