06bef18c8467026b49a36aec2bf1a73773524ef0
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-04-01 20:40:23 -0600

    "changes" :ws

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -7,16 +7,16 @@ function setDate() {
 
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    secondHand.style.transform = "rotate(" + secondDegrees + "deg)";
 
     
     const minutes = now.getMinutes();
     const minDegrees = ((minutes / 60) * 360) + 90;
-    minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+    minuteHand.style.transform = "rotate(" + minDegrees + "deg)";
 
     const hours = now.getHours();
     const hourDegrees = ((hours / 12) * 360) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    hourHand.style.transform = "rotate(" + hourDegrees + "deg)";
 
 }
 setInterval(setDate, 1000);
